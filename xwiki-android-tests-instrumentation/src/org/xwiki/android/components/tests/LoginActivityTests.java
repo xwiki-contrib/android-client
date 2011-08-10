edfb8c2852210e892918e8c1f93901d3638810d1
chamika <chamikaw@gmail.com>
2011-08-11 04:45:14 +0530

    Added simple instrumentation test

---
+++ xwiki-android-tests-instrumentation/src/org/xwiki/android/components/tests/LoginActivityTests.java
@@ -0,0 +1,23 @@
+package org.xwiki.android.components.tests;
+
+import org.xwiki.android.components.login.LoginActivity;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class LoginActivityTests extends ActivityInstrumentationTestCase2<LoginActivity>
+{
+    private Activity loginActivity;
+
+    public LoginActivityTests()
+    {
+        super("org.xwiki.android.tests", LoginActivity.class);
+    }
+
+    public void testSomething() throws Throwable
+    {
+        super.setUp();
+        loginActivity = this.getActivity();
+    }
+
+}
